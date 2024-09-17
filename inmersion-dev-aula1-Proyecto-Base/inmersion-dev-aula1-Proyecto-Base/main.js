a12d0638cf2a34259380e472e5dd7ecc5322266c
Kemon97 <kevin.montoya5146@uco.net.co>
2024-09-17 00:22:45 -0500

    First stpes to pasword generate

--- inmersion-dev-aula1-Proyecto-Base/inmersion-dev-aula1-Proyecto-Base/main.js
+++ inmersion-dev-aula1-Proyecto-Base/inmersion-dev-aula1-Proyecto-Base/main.js
@@ -1,3 +1,14 @@
+let cantidad = document.getElementById("cantidad");
+let boton = document.getElementById("generar");
+
+function GenerarContrasena(){
+
+    let numDigitado = parseInt(cantidad.value); 
+    if (numDigitado < 6){
+        alert("Debes ingresar más de 6 carácteres. Por favor intenta de nuevo");
+    }
+}
+
 
 
 
